4a45fb5c243837c323335b99f82478112542f4de
nazo6 <git@nazo6.dev>
2023-03-29 17:06:11 +0900

    Add GA

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import "./globals.css";
 
 export const metadata = {
@@ -13,11 +15,27 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className="h-full bg-gray-900">
+        <Script
+          async
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        />
+        <Script id="ga" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+        </Script>
         <h1 className="text-center">
           <span className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-500 ">
             Neovim Mine
           </span>
         </h1>
+
         {children}
       </body>
     </html>
