84b2f66384b68545266a1fb9843ae86d42f0e13a
nazo6 <git@nazo6.dev>
2023-05-03 13:54:40 +0900

    fix script

--- scripts/src/main.ts
+++ scripts/src/main.ts
@@ -10,69 +10,59 @@ import { mineRepos } from "./miner.js";
 
 dotenv.config();
 
-await main();
+const repos = await mineRepos([
+  "https://github.com/yutkat/my-neovim-pluginlist",
+  "https://github.com/rockerBOO/awesome-neovim",
+]);
+console.log("Repo count:" + repos.length);
 
-async function main() {
-  const repos = await mineRepos([
-    "https://github.com/yutkat/my-neovim-pluginlist",
-    "https://github.com/rockerBOO/awesome-neovim",
-  ]);
-  console.log("Repo count:" + repos.length);
+const { repos: resolvedRepos, notResolvedRepos: notResolvedRepos } =
+  await githubGql(repos);
 
-  const { repos: resolvedRepos, notResolvedRepos: notResolvedRepos } =
-    await githubGql(repos);
+const allRepos = [...notResolvedRepos, ...resolvedRepos];
 
-  const allRepos = [...notResolvedRepos, ...resolvedRepos];
-
-  // Remove duplicates
-  const filteredRepos = allRepos.filter(
-    (repo, i) => {
-      for (let j = i + 1; j < allRepos.length - 1; j++) {
-        if (
-          repo.name === allRepos[j + 1].name &&
-          repo.owner === allRepos[j + 1].owner &&
-          repo.domain === allRepos[j + 1].domain
-        ) {
-          return false;
-        }
-      }
-      return true;
-    },
-  );
-
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const dataPath = join(__dirname, "../../data/data");
-
-  await fs.rm(dataPath, { recursive: true, force: true });
-  const tasks = Promise.all(filteredRepos.map(async (repo) => {
-    const group = repo.owner.match(/[^0-9A-Za-z]*(?<c>.)/)?.groups?.c?.charAt(
-      0,
-    ).toLowerCase();
-    if (!group) throw Error;
-    const filePath = join(
-      dataPath,
-      sanitize(repo.domain),
-      group,
-      sanitize(`${repo.owner}_${repo.name}.json`),
-    );
-    try {
-      await fs.writeFile(filePath, stringify(repo, { space: 2 }));
-    } catch (e: unknown) {
-      if (typeof e == "object" && e && "code" in e && e.code === "ENOENT") {
-        await fs.mkdir(dirname(filePath), { recursive: true });
-        await fs.writeFile(filePath, stringify(repo, { space: 2 }));
-      } else {
-        console.log(e);
-        throw e;
+// Remove duplicates
+const filteredRepos = allRepos.filter(
+  (repo, i) => {
+    for (let j = i + 1; j < allRepos.length - 1; j++) {
+      if (
+        repo.name === allRepos[j + 1].name &&
+        repo.owner === allRepos[j + 1].owner &&
+        repo.domain === allRepos[j + 1].domain
+      ) {
+        return false;
       }
     }
-  }));
+    return true;
+  },
+);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dataPath = join(__dirname, "../../data/data");
+
+await fs.rm(dataPath, { recursive: true, force: true });
+await Promise.all(filteredRepos.map(async (repo) => {
+  const group = repo.owner.match(/[^0-9A-Za-z]*(?<c>.)/)?.groups?.c?.charAt(
+    0,
+  ).toLowerCase();
+  if (!group) throw Error;
+  const filePath = join(
+    dataPath,
+    sanitize(repo.domain),
+    group,
+    sanitize(`${repo.owner}_${repo.name}.json`),
+  );
   try {
-    await tasks;
-  } catch (e) {
-    console.error(e);
+    await fs.writeFile(filePath, stringify(repo, { space: 2 }));
+  } catch (e: unknown) {
+    if (typeof e == "object" && e && "code" in e && e.code === "ENOENT") {
+      await fs.mkdir(dirname(filePath), { recursive: true });
+      await fs.writeFile(filePath, stringify(repo, { space: 2 }));
+    } else {
+      console.log(e);
+      throw e;
+    }
   }
+}));
 
-  console.log("Done. " + filteredRepos.length + "repos");
-}
+console.log("Done. " + filteredRepos.length + "repos");
