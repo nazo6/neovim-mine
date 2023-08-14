7e4e6b78b89b9d37d6a203ee0d31e51315b2b47a
nazo6 <git@nazo6.dev>
2023-08-14 21:57:03 +0900

    fix metadata

--- site/app/(home)/page.tsx
+++ site/app/(home)/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Neovim Mine",
+  description: "Mine of neovim plugins and etc.",
+};
+
 export default async function Home() {
   return <></>;
 }
